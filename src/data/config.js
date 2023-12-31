4ffa7a4c3d578f10c4d9646abe738d852d247d13
Krrazy <serdalnazilli9@gmail.com>
2023-12-31 10:31:19 +0300

    owoteslimatototmatik
    owoteslimatototmatik

---
+++ src/data/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+
+    ACTIVITY_NAME: "12,90 ANLIK OWO FİYATI",
+    BOT_SAHIPLERI: ["900720172053180416"],
+    SELFBOT_TOKENS: ["", ""],
+    
+    DISCORD_BOT_TOKEN: "",
+    DISCORD_CLIENT_ID: "1188521902881390692",
+    MONGO_URI: "mongodb+srv://ghostmarketreal:ghostmarketreal@ghostmarketreal.opvnlrw.mongodb.net/?retryWrites=true&w=majority", // değiştirmenize gerek yok
+    
+    SUNUCU_ID: "1188554482636837085",
+    TESLIMAT_KANALI_ID: "1188559496331669554",
+    LOG_KANALI_ID: "1188556226854932510",
+    LOG_CHANNEL_WEBHOOK_LINK: "https://discord.com/api/webhooks/1188598403639296101/u-uN7gndmaJDJoCJ2oMKfFVIr2i9rb3SMkSraviW0gUG9EmF5Xeltngue_YT3LCml06T",
+    SISTEM_LOG_KANALI_ID: "1188556226854932510",
+    KOD_YAZILACAK_TESLIM_KANALI: "1188557536253710487",
+
+    TESLIMAT_SEKLI: "owo give {user} {cash}",
+    WEBHOOK_KOD_ONAYLANDI_BILDIRIMI: "# <a:shop_yes:1188589092854845492> Kod onaylandı {member}, lütfen bekleyin.",
+    WEBHOOK_TESLIM_BILDIRIMI: "<a:ghost_heart:1190917899829923870> __İşlem tamamlandı,__ **owo cash** __yazarak gelip gelmediğini kontrol edin.__\n\n<a:ghost_heart:1190917899829923870> **Siparişinizi onaylayıp değerlendirme yapmayı unutmayın!**",
+    WEBHOOK_HATA_BILDIRIMI: "Paranız şu anda gönderilemiyor, lütfen size yardımcı olması için bir yetkiliyi etiketleyin.",
+
+    OWO_BOT_ID: "408785106942164992",
+
+};
---
+++ start.bat
@@ -0,0 +1,7 @@
+echo off
+title OwO Teslimat Botu By GhostMarket İtem Satış
+cls
+:a
+node .
+goto a
+pause
